cb14dee3f0ea2e215f253dbb9891402bfdced78c
justinpeter <justinhasanaccount@gmail.com>
2019-04-13 18:36:30 +0200

    package

--- setup.py
+++ setup.py
@@ -5,17 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="quantum-dice",
-    version="0.3",
+    version="0.3.2",
     author="Justin Peter",
-    author_email="justinhasanaccount@gmail.com",
+    author_email="planetzzyx@protonmail.com",
     description="A dice using quantum mechanics.",
     long_description="The Quantum Dice leverages the IBM Q quantum computer to generate a truly random set of numbers.",
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/TheMagicNacho/quantum-dice",
     packages=setuptools.find_packages(),
+    install_requires = ['qiskit'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
 )
